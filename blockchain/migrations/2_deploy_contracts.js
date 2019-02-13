c510857d244efb9c29547cfe692cbcb2975b6acc
lotusiroh <igbonilla@deloitte.com>
2019-02-12 23:07:02 -0300

    First commit on ERC20 contract

--- blockchain/migrations/2_deploy_contracts.js
+++ blockchain/migrations/2_deploy_contracts.js
@@ -1,8 +1,11 @@
-let Lottery = artifacts.require('./Lottery.sol')
-let Donations = artifacts.require('./Donations.sol')
-const OWNER = web3.eth.accounts[5]
-const INITIAL_JACKPOT = web3.toWei(50, 'ether')
-const SEED = 87
+let Lottery = artifacts.require('./Lottery.sol');
+let Donations = artifacts.require('./Donations.sol');
+let DVC = artifacts.require('./DVC.sol');
+
+const OWNER = web3.eth.accounts[5];
+const INITIAL_JACKPOT = web3.toWei(1, 'ether');
+const SEED = 87;
+const initialSupply = 1;
 
 module.exports = function(deployer) {
     deployer.deploy(Lottery, SEED, {
@@ -11,6 +14,7 @@ module.exports = function(deployer) {
     })
     deployer.deploy(Donations, {
         from: OWNER,
-        value: web3.toWei(10, 'ether')
+        value: web3.toWei(0.1, 'ether')
     })
+    deployer.deploy(DVC, initialSupply)
 }
