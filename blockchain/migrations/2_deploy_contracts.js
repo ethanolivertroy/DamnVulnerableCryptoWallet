0d9c5745af88d8a45ab8373bf6b5013527b2fc32
igbonilla <igbonilla@deloitte.com>
2019-04-05 13:35:19 -0300

    Updated Deploy script

--- blockchain/migrations/2_deploy_contracts.js
+++ blockchain/migrations/2_deploy_contracts.js
@@ -2,15 +2,15 @@ let DVCToken = artifacts.require('./DVCToken.sol');
 let DVCTokenSale = artifacts.require('DVCTokenSale.sol');
 const fs = require('fs');
 
-const OWNER = web3.eth.accounts[9];
+const ownerDVC = web3.eth.accounts[7];
+const ownerDVCSale = web3.eth.accounts[9];
 const tokensSold = 0;
-const network = 'development'
 const initialSupply = 1000000;
 
 module.exports = function(deployer, accounts) {
-    deployer.deploy(DVCToken, initialSupply, {from: OWNER}).then(function () {
+    deployer.deploy(DVCToken, initialSupply, {from: ownerDVC, value: 90000000000000000000}).then(function () {
      tokenPrice = 1000000000000000000;
-      return deployer.deploy(DVCTokenSale, DVCToken.address, tokenPrice, tokensSold).then(function () {
+     return deployer.deploy(DVCTokenSale, DVCToken.address, tokenPrice, tokensSold, {from: ownerDVCSale}).then(function () {
         fs.writeFile('contractAddress.txt', DVCToken.address + ',' + DVCTokenSale.address, function (err) {
           if (err) throw err;
         }); 
